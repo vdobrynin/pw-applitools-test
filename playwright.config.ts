573ec42e56d9d6fb1b24cb78253d2f1111da3952
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-20 17:38:05 -0700

    #75 visual testing with applitools part 1.0

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,28 @@
 import { EyesFixture } from '@applitools/eyes-playwright/fixture';
 import { defineConfig, devices } from '@playwright/test';
+import { MatchLevel, StitchMode } from '@applitools/eyes-playwright';
 
 export default defineConfig<EyesFixture>({
-
-  reporter: '@applitools/eyes-playwright/reporter',
+  // reporter: 'html',                                      // #75.1 first test
+  reporter: '@applitools/eyes-playwright/reporter',  // #75.1 then change
 
   use: {
     /* Configuration for Eyes VisualAI */
     eyesConfig: {
       /* The following and other configuration parameters are documented at: <url> */
-      apiKey: '', // alternatively, set this via environment variable APPLITOOLS_API_KEY
+      apiKey: 'PfOl8NHYrjt108PYZSqybag00UOOqG16zROdRo4QVIFvc110', // alternatively, set this via environment variable APPLITOOLS_API_KEY
+      // apiKey: process.env.APPLITOOLS_API_KEY,
+      // serverUrl: 'https://eyes.applitools.com',
 
+      // failTestsOnDiff: 'false',
+      // appName: 'My App',
+      // matchLevel: 'Strict',
+      // batch: { name: 'My batch' },
+      // proxy: '{url:http://127.0.0.1:8888',
+      // stitchMode: 'CSS',
+      // matchTimeout: '0',
+      // waitBeforeScreenshots: 50,
+      // saveNewTests: true,
     },
 
     baseURL: 'http://localhost:4200',
@@ -23,5 +35,4 @@ export default defineConfig<EyesFixture>({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-
 });
--- tests/applitoolsTest.spec.ts
+++ tests/applitoolsTest.spec.ts
@@ -1,6 +1,8 @@
-import { test } from '@applitools/eyes-playwright/fixture';
+// import { test, expect } from '@playwright/test';             // #75.1 at first test
+import { test } from '@applitools/eyes-playwright/fixture';     //#75.1 later
 
-test('Applitools VisualTesting', async ({ page, eyes }) => {
+test('Applitools VisualTesting', async ({ page, eyes }) => {      //#75.1 later
+// test('Applitools VisualTesting', async ({ page }) => {            // #75.1 at first test
   await page.goto('/');
 
   await eyes.check('Home Page')
